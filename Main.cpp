58d107148bb244cc2f2d88511c0c128caeb64ff6
MarcoGr11 <babyn.bohdan.o@chnu.edu.ua>
2024-03-03 23:09:58 +0200

    little change in code and commets

--- Main.cpp
+++ Main.cpp
@@ -5,27 +5,27 @@
 using namespace std; 
 
 int main() {
-    // Створення екземпляру Student з іменем 
+    // створення екземпляру студ 1
     Student student1("John Doe", 20, "S12345");
-    // Створення ще одного екземпляру Student 
+    // другий екземпляр 
     Student student2("Jane Doe", 19, "S12346");
 
-    // Створення екземпляру StudentGroup з назвою "Group A"
+    // створ екземпляру студент групи A
     StudentGroup group1("Group A");
-    // Додавання student1 до групи group1
+    // додаван студ1 до групи група1
     group1.addStudent(student1);
-    // Додавання student2 до групи group1
+    // додаван студ2 до групи група1
     group1.addStudent(student2);
 
-    // Створення екземпляру курсу Course з назвою курсу "Introduction to Programming", кодом курсу "CS101" та 4 кредитами
+    // створ екземпляру курсу  
     Course course1("Introduction to Programming", "CS101", 4);
 
-    // Виведення деталей про student1 за допомогою методів getName() та getId()
+    // вивед деталей про студ1 за допомогою методів getName() та getId()
     cout << "Student: " << student1.getName() << ", ID: " << student1.getId() << endl;
-    // Виведення назви групи group1 за допомогою методу getGroupName()
+    // вивед деталей про студ1 за допомогою методу getGroupName()
     cout << "Group: " << group1.getGroupName() << endl;
-    // Виведення деталей про курс course1 за допомогою методів getCourseName() та getCredits()
+    // виведення деталей про курс1 використ методи getCourseName() та getCredits()
     cout << "Course: " << course1.getCourseName() << ", Credits: " << course1.getCredits() << endl;
 
-    return 0; // Кінець головної функції
+    return 0; // кінець...
 }
