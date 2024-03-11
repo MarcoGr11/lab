3071a8ede315ac93577985d186cad38daa987d71
MarcoGr11 <babyn.bohdan.o@chnu.edu.ua>
2024-03-11 23:03:09 +0200

    Change

--- Student.cpp
+++ Student.cpp
@@ -1,102 +1,39 @@
 #include "Student.h"
-// ініціалізація статичної змінної
 
-int Student::studentCount = 0; 
+// делегування конструктора 
+Student::Student() : Student("Unknown", 0, "0000") {}
 
-// конструктор за замовч
-Student::Student() : Student("Unknown", 0, "0000") {
-    studentCount++;
-}
+// перевантажений конструктор для ініціаліз
+Student::Student(string name, int age, string id) : name(name), age(age), id(id) {}
 
-// параметризований конструктор
-Student::Student(string name, int age, string id) : name(name), age(age), id(id) {
-    studentCount++;
-}
+Student::~Student() {} // деструктор
 
-// конструктор копіювання
-Student::Student(const Student& other) : name(other.name), age(other.age), id(other.id) {
-    studentCount++;
-}
-
-// конструктор переміщ
-Student::Student(Student&& other) noexcept : name(move(other.name)), age(other.age), id(move(other.id)) {
-    studentCount++;
-}
-
-// деструктор
-Student::~Student() {
-    studentCount--;
-}
-
-// оператор присвоєння копіювання
-Student& Student::operator=(const Student& other) {
-    if (this != &other) {
-        name = other.name;
-        age = other.age;
-        id = other.id;
-    }
-    return *this;
-}
-
-// оператор присвоєння переміщ
-Student& Student::operator=(Student&& other) noexcept {
-    if (this != &other) {
-        name = move(other.name);
-        age = other.age;
-        id = move(other.id);
-    }
-    return *this;
-}
-
-// унарний оператор 
-Student& Student::operator++() {
-    ++age;
-    return *this;
-}
-// гетери та сетери
+// метод для встанов імені студ
 void Student::setName(string name) {
-    this->name = name;
+    this->name = name; // зберігаємо передане ім'я у змінну name класу stud
 }
 
+// метод для отримання імені студента.
 string Student::getName() const {
-    return name;
+    return name; // повертаємо значення змінної name  класу stud
 }
 
+// метод для встановлення віку студента.
 void Student::setAge(int age) {
-    this->age = age;
+    this->age = age; // хзберігаємо переданий вік у змінну age класу student.
 }
 
+// метод для отримання віку студе
 int Student::getAge() const {
-    return age;
+    return age; // повертаємо значення змінної age класу student.
 }
 
+// метод для встановлення ідентифікатора студента.
 void Student::setId(string id) {
-    this->id = id;
+    this->id = id; // зберігаємо переданий ідентифікатор у змінну id класу student.
 }
 
+// метод для отримання ідентифікатора студента.
 string Student::getId() const {
-    return id;
+    return id; // повертаємо значення змінної id класу student.
 }
-
-// статичний метод для отрим к-ті студент
-int Student::getStudentCount() {
-    return studentCount;
-}
-
-// оператор вивед
-ostream& operator<<(ostream& os, const Student& student) {
-    os << "Name: " << student.name << ", Age: " << student.age << ", ID: " << student.id;
-    return os;
-}
-
-// оператор введ
-istream& operator>>(istream& is, Student& student) {
-    cout << "Enter name: ";
-    is >> student.name;
-    cout << "Enter age: ";
-    is >> student.age;
-    cout << "Enter ID: ";
-    is >> student.id;
-    return is;
-}
-
